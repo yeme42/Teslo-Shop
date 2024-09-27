9aae13001d8f1e76066ffd4b8000f9d658ee41ad
yeme42 <yerindd@gmail.com>
2024-09-27 13:01:55 -0300

    cambio en puerto

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,7 @@ import { CommonModule } from './common/common.module';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: +process.env.DB_HOST,
+      port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
