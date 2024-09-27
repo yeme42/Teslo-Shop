a276f66f9df88312669edae8bc5a71841e889648
yeme42 <yerindd@gmail.com>
2024-09-27 12:03:30 -0300

    Backend en nest js

---
+++ README.md
@@ -0,0 +1,14 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+</p>
+
+# Teslo API
+
+1. Clonar proyecto
+2. ```yarn install```
+3. Clonar el archivo ```.env.template``` y renombrarlo a ```.env```
+4. Levantar la base de datos 
+```
+docker-compose up -d
+```
+5. Levantar : ```yarn start:dev```
---
+++ src/products/products.service.ts
@@ -0,0 +1,109 @@
+import { BadRequestException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { NotFoundError } from 'rxjs';
+import { PaginationDto } from 'src/common/dtos/pagitation.dto';
+
+import { Product } from './entities/product.entity';
+import { validate as isUUID } from 'uuid';
+
+@Injectable()
+export class ProductsService {
+
+private readonly logger = new Logger('ProdcutService')
+
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ){}
+
+  async create(createProductDto: CreateProductDto) {
+
+    try{
+
+      const product = this.productRepository.create(createProductDto);
+      await this.productRepository.save(product);
+
+      return product;
+    }catch(error){
+
+        this.handDBExceptions(error)
+    }
+  }
+
+  findAll(paginationDto: PaginationDto) {
+
+    const {limit =  10, offset = 0 } = paginationDto
+
+    return this.productRepository.find({
+      take: limit,
+      skip: offset
+
+    })
+  }
+
+  async findOne(term: string) {
+
+    let product: Product;
+
+    if (isUUID(term)){
+      product = await this.productRepository.findOneBy({ id: term})
+    } else{
+      const queryBuilder = this.productRepository.createQueryBuilder()
+      product = await queryBuilder
+        .where('UPPER(title) =:title or slug =:slug',{
+          title: term.toUpperCase(),
+          slug: term.toLowerCase(),
+        }).getOne()
+    }
+
+      if (!product)
+        throw new NotFoundException(`Product with  ${term} not found`);
+      
+      return product;
+      
+
+  }
+
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    
+    const product = await this.productRepository.preload({
+      id:id,
+      ...updateProductDto
+    })
+
+    if (!product)
+      throw new NotFoundException (`Product with id: ${id} not found`)
+    
+    try{
+      
+      await  this.productRepository.save(product);
+      return product;
+
+    }catch(error){
+
+      this.handDBExceptions(error)
+    }
+
+
+
+  }
+
+  async remove(id: string) {
+      const product = await this.findOne(id)
+      
+      await this.productRepository.remove(product)
+  }
+
+  private handDBExceptions(error: any){
+
+    if (error.code === '23505')
+      throw new BadRequestException(error.detail);
+
+    this.logger.error(error)      
+    throw new InternalServerErrorException('Undexpectd error, check server logs')
+  }
+
+}
